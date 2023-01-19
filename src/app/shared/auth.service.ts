b856bfde9a65d11f8ab88b7abd2cef95197e4075
Eltach00 <eltacquliyev@gmail.com>
2023-01-19 18:04:24 +0400

    firebase configuration

--- src/app/shared/auth.service.ts
+++ src/app/shared/auth.service.ts
@@ -15,3 +15,23 @@ export class AuthService {
     );
   }
 }
+
+/*
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyB3alSKzL6U-pBJRW136H8oFZR3aFKlcQE",
+  authDomain: "online-shop-8e18a.firebaseapp.com",
+  projectId: "online-shop-8e18a",
+  storageBucket: "online-shop-8e18a.appspot.com",
+  messagingSenderId: "286762524183",
+  appId: "1:286762524183:web:2a4b6423ad4d7493ffc652"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+ */
